9dc4eabcc9a0becc8887a5e15af3be0df6d45505
Youssef-Mohammed317 <youssefmohammad040317@gmail.com>
2026-02-27 14:17:54 +0200

    add log to file and prepare for app insights

--- HR.LeaveManagement.API/Program.cs
+++ HR.LeaveManagement.API/Program.cs
@@ -1,20 +1,31 @@
 using HR.LeaveManagement.API.Extensions;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 
 Log.Logger = new LoggerConfiguration()
-    .MinimumLevel.Information()
-    .WriteTo.Console()
+    .ReadFrom.Configuration(new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json")
+        .Build())
     .CreateLogger();
 
 try
 {
+    Log.Information("Starting HR.LeaveManagement API...");
+
     var builder = WebApplication.CreateBuilder(args);
 
-    builder.Host.UseSerilog();
+    builder.Host.UseSerilog((context, services, configuration) =>
+    {
+        configuration.ReadFrom.Configuration(context.Configuration);
+    });
 
     builder.Services.AddApiServices(builder.Configuration);
 
     var app = builder.Build();
+
+    Log.Information("Application started successfully in {Environment}",
+    app.Environment.EnvironmentName);
 
     await app.InitialiseDatabaseAsync();
 
