331edd9129a5309e96ff5a2687fa035b57e99e7a
Youssef-Mohammed317 <youssefmohammad040317@gmail.com>
2026-02-27 13:22:14 +0200

    add integration test and end the solid course

---
+++ HR.LeaveManagement.Persistence.Integration.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace HR.LeaveManagement.Persistence.Integration.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ HR.LeaveManagement.Persistence.Tests/DatabaseContext/HrDatabaseContextTests.cs
@@ -0,0 +1,65 @@
+﻿using HR.LeaveManagement.Application.Contracts.Identity;
+using HR.LeaveManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace HR.LeaveManagement.Persistence.DatabaseContext.Tests
+{
+    public class HrDatabaseContextTests
+    {
+        private HrDatabaseContext context;
+
+        public HrDatabaseContextTests()
+        {
+            var dbOptions = new DbContextOptionsBuilder<HrDatabaseContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+            var currentUserMock = new Mock<ICurrentUserService>();
+            currentUserMock.Setup(x => x.UserId).Returns("TestUser");
+            context = new HrDatabaseContext(dbOptions, currentUserMock.Object);
+        }
+        [Fact()]
+        public async Task SaveChangesAsync_Should_Set_Audit_Fields()
+        {
+            var leaveType = new LeaveType
+            {
+                DefaultDays = 7,
+                Name = "Test Vacation"
+            };
+
+            await context.LeaveTypes.AddAsync(leaveType);
+            await context.SaveChangesAsync();
+
+            leaveType.DateCreated.ShouldNotBeNull();
+            leaveType.DateModified.ShouldNotBeNull();
+            leaveType.DateModified.Value.ToShortDateString().ShouldBe(leaveType.DateCreated.Value.ToShortDateString());
+            leaveType.CreatedBy.ShouldBe("TestUser");
+            leaveType.ModifiedBy.ShouldBe("TestUser");
+        }
+        [Fact()]
+        public async Task SaveChangesAsync_Should_adjust_Audit_Fields()
+        {
+            var leaveType = new LeaveType
+            {
+                DefaultDays = 16,
+                Name = "Test Vacation"
+            };
+
+            // Add
+            await context.LeaveTypes.AddAsync(leaveType);
+            await context.SaveChangesAsync();
+
+            var createdDate = leaveType.DateCreated.Value;
+
+            // Modify
+            leaveType.DefaultDays = 20;
+            await context.SaveChangesAsync();
+
+            leaveType.DateModified.ShouldNotBeNull();
+            leaveType.DateModified.Value.ShouldBeGreaterThan(createdDate);
+            leaveType.ModifiedBy.ShouldBe("TestUser");
+        }
+    }
+}
